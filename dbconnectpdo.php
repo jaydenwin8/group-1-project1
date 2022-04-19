9996e13803ce0bd52033f0c2b2453f6ed39c9962
scottchestnut <83614175+scottchestnut@users.noreply.github.com>
2022-04-18 21:43:54 -0700

    Update dbconnectpdo.php

--- dbconnectpdo.php
+++ dbconnectpdo.php
@@ -7,7 +7,6 @@ try
 {
     $conn=new PDO("mysql:host=$servername;dbname=sprog20221",$username,$password);
     $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    echo 'connected';
 }
 catch(PDOException $e)
 {
